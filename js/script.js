e1fdf2ab96d4fc9b835a40655800cf10965b9a56
MurphyDownpour <azaur.p302@code.edu.az>
2017-06-20 20:47:54 +0400

    Animations

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function($) {
+	$("#templateName").fadeIn(600, function() {
+		$("#first").fadeIn(600, function() {
+			$("#second").fadeIn(600, function() {
+				$("#third").fadeIn(600, function(){
+					$(".btn-group button").fadeIn(1000, function() {
+						$("form").fadeIn(600);
+					});
+				});
+			});
+		});
+	});
+});
