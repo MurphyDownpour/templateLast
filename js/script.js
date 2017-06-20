b248d90ffe3a18eef78128d2f004c1c1cbb1b635
MurphyDownpour <azaur.p302@code.edu.az>
2017-06-20 21:19:54 +0400

    A few changes

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function($) {
 		$("#first").fadeIn(600, function() {
 			$("#second").fadeIn(600, function() {
 				$("#third").fadeIn(600, function(){
-					$(".btn-group button").fadeIn(1000, function() {
-						$("form").fadeIn(600);
+					$(".btn-group button").fadeIn(600, function() {
+						$("form").show(600);
 					});
 				});
 			});
